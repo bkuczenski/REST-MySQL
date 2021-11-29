766f8a5d1cfd70ecfd0e453223e4bef2c8c69ebd
icik251 <hristostanulov@gmail.com>
2021-11-29 12:03:08 -0800

    env vars optimization

--- app/database.py
+++ app/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@localhost/blackbook_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://{}:{}@{}/{}".format(os.getenv('MYSQL_ROOT_USER'), os.getenv('MYSQL_ROOT_PASSWORD'), 
+                                                                    os.getenv('MYSQL_HOST'), db=os.getenv('MYSQL_DB_NAME'))
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
